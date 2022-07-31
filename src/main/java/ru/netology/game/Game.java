0505d695902da41a2686d4e83fa5b949bab25fa4
Aliaksandr Zuyeuski <negrustin87@gmail.com>
2022-07-31 15:56:54 +0200

    install Tournament Manager

---
+++ src/main/java/ru/netology/game/Game.java
@@ -0,0 +1,45 @@
+package ru.netology.game;
+
+import ru.netology.domain.Player;
+import ru.netology.exceptions.NotRegisteredException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+    List<Player> playersList = new ArrayList<>();
+
+    public List<Player> getPlayersList() {
+        return playersList;
+    }
+
+    public void register(Player player) {
+        playersList.add(player);
+    }
+
+    public int round(String playerName1, String playerName2) {
+        int result = 0;
+        if (findByName(playerName1) == null){
+            throw new NotRegisteredException("Player " + playerName1 + " is not registered");
+        }
+        if (findByName(playerName2) == null){
+            throw new NotRegisteredException("Player " + playerName1 + " is not registered");
+        }
+        if(findByName(playerName1).getStrength() > findByName(playerName2).getStrength()){
+            return 1;
+        }else if(findByName(playerName1).getStrength() < findByName(playerName2).getStrength()){
+            return 2;
+        }else {
+            return 0;
+        }
+    }
+
+    public Player findByName(String name) {
+        for (Player player : playersList) {
+            if (player.getName() == name) {
+                return player;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/test/java/ru/netology/game/GameTest.java
@@ -0,0 +1,96 @@
+package ru.netology.game;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Player;
+import ru.netology.exceptions.NotRegisteredException;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class GameTest {
+
+    Player player1 = new Player(1, "Name1", 60);
+    Player player2 = new Player(2, "Name2", 50);
+    Player player3 = new Player(3, "Name3", 70);
+    Player player4 = new Player(4, "Name4", 60);
+
+    @Test
+    public void registered() {
+
+
+    }
+
+    @Test
+    public void firstPlayerIsWinnRound() {
+        Game game = new Game();
+        game.register(player1);
+        game.register(player2);
+
+
+        int excepted = 1;
+        int actual = game.round("Name1", "Name2");
+
+        Assertions.assertEquals(excepted, actual);
+    }
+
+    @Test
+    public void secondPlayerIsWinnRound() {
+        Game game = new Game();
+        game.register(player1);
+        game.register(player3);
+
+        int excepted = 2;
+        int actual = game.round("Name1", "Name3");
+
+        Assertions.assertEquals(excepted, actual);
+    }
+
+    @Test
+    public void nitherSideWinnRound() {
+        Game game = new Game();
+        game.register(player1);
+        game.register(player4);
+
+        int excepted = 0;
+        int actual = game.round("Name1", "Name4");
+
+        Assertions.assertEquals(excepted, actual);
+    }
+
+    @Test
+    public void firstPlayerIsNotRegistered() {
+        Game game = new Game();
+        game.register(player1);
+        game.register(player2);
+
+        Assertions.assertThrows(NotRegisteredException.class, () -> {
+            game.round("Name1", "Name3");
+        });
+    }
+
+    @Test
+    public void secondPlayerIsNotRegistered() {
+        Game game = new Game();
+        game.register(player1);
+        game.register(player2);
+
+        Assertions.assertThrows(NotRegisteredException.class, () -> {
+            game.round("Name4", "Name2");
+        });
+    }
+
+    @Test
+    public void registerPlayers() {
+        Game game = new Game();
+        game.register(player1);
+        game.register(player2);
+
+        List<Player> expected = Arrays.asList(player1, player2);
+        List<Player> actual = game.getPlayersList();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
+
